001bbfadafc95c9cdf6c842d8f55ccc76235f834
carmenteearu@gmail.com <carmenteearu@gmail.com>
2020-02-12 22:14:34 +0200

    Add project files.

---
+++ MVC_ContosoUniversity/Models/Course.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Security.Permissions;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace MVC_ContosoUniversity.Models
+{
+    public class Course
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int CourseID { get; set; }
+        public string Title { get; set; }
+        public int Credits { get; set; }
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
