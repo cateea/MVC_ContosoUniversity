cc730c4d72f73bf15d43e29df6810868722cbd21
carmenteearu@gmail.com <carmenteearu@gmail.com>
2020-02-16 17:25:19 +0200

    Töötav kood enne seed database

--- MVC_ContosoUniversity/Models/Enrollment.cs
+++ MVC_ContosoUniversity/Models/Enrollment.cs
@@ -1,14 +1,19 @@
-﻿namespace MVC_ContosoUniversity.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MVC_ContosoUniversity.Models
 {
     public enum Grade
     {
-        A, B, C, D, E, F
+        A, B, C, D, F
     }
+
     public class Enrollment
     {
         public int EnrollmentID { get; set; }
         public int CourseID { get; set; }
         public int StudentID { get; set; }
+        [DisplayFormat(NullDisplayText = "No grade")]
         public Grade? Grade { get; set; }
 
         public Course Course { get; set; }
